e819e38af3a66edd6c079cdb043153a926b5134b
Aleksey <fduchunru@gmail.com>
2020-09-26 03:12:49 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,23 +1,14 @@
 function basicTeenager(age) {
-  if (age >=13 && age <=19)
-  {return "You are a teenager!"}
-
-return age
+  
 }
 
 function teenager(age) {
-  if(age >=13 && age <=19)
-  {return "You are a teenager!"}
-  return "You are not a teenager"
+  
 
 }
 
 function ageChecker(age) {
-  if(age >=13 $$ age <=19)
-  {return "You are a teenager!"}
-  else if (age <=12)
-  {return "You are a kid!"}
-  return "You are grownup"
+  
 
 }
 
